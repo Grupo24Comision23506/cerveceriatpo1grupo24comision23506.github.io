7c7d89dc08c14aa379c88450efaac57d127feb74
Grupo24 <148916949+oliverexx@users.noreply.github.com>
2023-10-28 20:56:22 -0300

    Modifique historia

--- assets/js/Tarjetas.js
+++ assets/js/Tarjetas.js
@@ -1,14 +1,27 @@
-// Iniciar con la primera imagen visible
-images[currentImageIndex].style.opacity = 1;
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const sliderImages = document.querySelectorAll(".slider-image");
 
-document.addEventListener("DOMContentLoaded", function () {
-    let currentImageIndex = 0;
-    const images = document.querySelectorAll("#image-tarjetas img");
-    function changeImage(direction) {
-        images[currentImageIndex].style.opacity = 0;
-        currentImageIndex = (currentImageIndex + direction + images.length) % images.length;
-        images[currentImageIndex].style.opacity = 1;
-    }
-       // Iniciar con la primera imagen visible
-    images[currentImageIndex].style.opacity = 1;
+let currentImageIndex = 0;
+
+function showImage(index) {
+    sliderImages.forEach((image, i) => {
+        if (i === index) {
+            image.style.display = "block";
+        } else {
+            image.style.display = "none";
+        }
+    });
+}
+
+showImage(currentImageIndex);
+
+prevBtn.addEventListener("click", () => {
+    currentImageIndex = (currentImageIndex - 1 + sliderImages.length) % sliderImages.length;
+    showImage(currentImageIndex);
+});
+
+nextBtn.addEventListener("click", () => {
+    currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
+    showImage(currentImageIndex);
 });
