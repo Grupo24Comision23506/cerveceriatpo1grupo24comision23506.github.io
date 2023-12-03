5b1b71e663e726c0357d25fe2f8ec40a3e88d84d
Gaston Chol <136974503+codebeerlover@users.noreply.github.com>
2023-12-03 15:13:06 -0300

    Limpiando Codigo
    CHOL GASTON

--- app_products/router.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .viewsets import ProductViewSet, ImageViewSet
-
-router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='product')
-router.register(r'images', ImageViewSet, basename='image')
-
